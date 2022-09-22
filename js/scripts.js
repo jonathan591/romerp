04a8ccbf95fbe03b996226789fee55858ffed6ac
jonathan591 <77215819+jonathan591@users.noreply.github.com>
2022-09-22 11:51:39 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+window.addEventListener('load',() =>{
+    const contenedor_loader = document.querySelector('.contenedor_loader');
+    contenedor_loader.style.opacity=0;
+    contenedor_loader.style.visibility='hidden'
+
+})
+//  Typed with Lyrics
+var typed = new Typed('.typing', {
+    strings: ["H.R Construction", "", "H.R Construction"],
+    typeSpeed: 100,
+    backDelay: 5000,
+    backSpeed: 100,
+    loop: true,
+});
